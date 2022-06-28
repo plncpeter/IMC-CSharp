b4b86d6afc0717e2b9d55c5296e8cab57ef994c0
plncpeter <palenciocraft@gmail.com>
2022-06-28 20:10:35 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace A4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblNome1 = new System.Windows.Forms.Label();
+            this.lblNome2 = new System.Windows.Forms.Label();
+            this.lblNome3 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblConfirmacao = new System.Windows.Forms.Label();
+            this.txbN1 = new System.Windows.Forms.TextBox();
+            this.txbN2 = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(68, 31);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(84, 13);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Calcule seu IMC";
+            // 
+            // lblNome1
+            // 
+            this.lblNome1.AutoSize = true;
+            this.lblNome1.Location = new System.Drawing.Point(28, 114);
+            this.lblNome1.Name = "lblNome1";
+            this.lblNome1.Size = new System.Drawing.Size(31, 13);
+            this.lblNome1.TabIndex = 1;
+            this.lblNome1.Text = "Peso";
+            // 
+            // lblNome2
+            // 
+            this.lblNome2.AutoSize = true;
+            this.lblNome2.Location = new System.Drawing.Point(28, 227);
+            this.lblNome2.Name = "lblNome2";
+            this.lblNome2.Size = new System.Drawing.Size(34, 13);
+            this.lblNome2.TabIndex = 2;
+            this.lblNome2.Text = "Altura";
+            this.lblNome2.Click += new System.EventHandler(this.lblNome2_Click);
+            // 
+            // lblNome3
+            // 
+            this.lblNome3.AutoSize = true;
+            this.lblNome3.Location = new System.Drawing.Point(28, 342);
+            this.lblNome3.Name = "lblNome3";
+            this.lblNome3.Size = new System.Drawing.Size(26, 13);
+            this.lblNome3.TabIndex = 3;
+            this.lblNome3.Text = "IMC";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(157, 342);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(29, 13);
+            this.lblResultado.TabIndex = 4;
+            this.lblResultado.Text = "NaN";
+            // 
+            // lblConfirmacao
+            // 
+            this.lblConfirmacao.AutoSize = true;
+            this.lblConfirmacao.Font = new System.Drawing.Font("HoloLens MDL2 Assets", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConfirmacao.ForeColor = System.Drawing.Color.Chocolate;
+            this.lblConfirmacao.Location = new System.Drawing.Point(27, 404);
+            this.lblConfirmacao.Name = "lblConfirmacao";
+            this.lblConfirmacao.Size = new System.Drawing.Size(68, 21);
+            this.lblConfirmacao.TabIndex = 5;
+            this.lblConfirmacao.Text = "Situação";
+            // 
+            // txbN1
+            // 
+            this.txbN1.Location = new System.Drawing.Point(86, 111);
+            this.txbN1.Name = "txbN1";
+            this.txbN1.Size = new System.Drawing.Size(100, 20);
+            this.txbN1.TabIndex = 6;
+            // 
+            // txbN2
+            // 
+            this.txbN2.Location = new System.Drawing.Point(86, 224);
+            this.txbN2.Name = "txbN2";
+            this.txbN2.Size = new System.Drawing.Size(100, 20);
+            this.txbN2.TabIndex = 7;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(86, 291);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(100, 23);
+            this.btnCalcular.TabIndex = 9;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(231, 459);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txbN2);
+            this.Controls.Add(this.txbN1);
+            this.Controls.Add(this.lblConfirmacao);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblNome3);
+            this.Controls.Add(this.lblNome2);
+            this.Controls.Add(this.lblNome1);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblNome1;
+        private System.Windows.Forms.Label lblNome2;
+        private System.Windows.Forms.Label lblNome3;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblConfirmacao;
+        private System.Windows.Forms.TextBox txbN1;
+        private System.Windows.Forms.TextBox txbN2;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblNome2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            // Variaveis p/ armazenar os valores 
+            double peso = 0;
+            double altura = 0;
+            double imc = 0;
+            if (txbN1.Text != "" && txbN2.Text != "")
+            {
+                peso = double.Parse(txbN1.Text);
+                altura = double.Parse(txbN2.Text);
+            }
+                
+           
+            // Fazer a operação:
+            imc = peso / (altura * altura);
+
+            // Mostrar o valor no lblResult:
+            lblResultado.Text = imc.ToString();
+
+            //Mostrar sua situação:
+            if (double.Parse(lblResultado.Text) <= 18.5)
+            {
+                lblConfirmacao.Text = "Abaixo do peso";
+                lblConfirmacao.ForeColor = Color.Red;
+            }
+            else if (double.Parse(lblResultado.Text) >= 18.6 && (double.Parse(lblResultado.Text) <= 24.9))
+            {
+                lblConfirmacao.Text = "Peso Ideal";
+                lblConfirmacao.ForeColor = Color.Green;
+            }
+            else if (double.Parse(lblResultado.Text) >= 25 && (double.Parse(lblResultado.Text) <= 29.9))
+            {
+                lblConfirmacao.Text = "Levemente acima do peso";
+                lblConfirmacao.ForeColor = Color.Yellow;
+            }
+            else if (double.Parse(lblResultado.Text) >= 30 && (double.Parse(lblResultado.Text) <= 34.9))
+            {
+                lblConfirmacao.Text = "Obesidade Grau 1";
+                lblConfirmacao.ForeColor = Color.Red;
+            }
+            else if (double.Parse(lblResultado.Text) >= 35 && (double.Parse(lblResultado.Text) <= 39.9))
+            {
+                lblConfirmacao.Text = "Obesidade Grau 2";
+                lblConfirmacao.ForeColor = Color.Red;
+            }
+            else if (double.Parse(lblResultado.Text) >= 40)
+            {
+                lblConfirmacao.Text = "Obesidade Grau 3";
+                lblConfirmacao.ForeColor = Color.Red;
+            }
+            else
+            {
+                lblConfirmacao.Text = "Verifique os dados";
+                lblConfirmacao.ForeColor = Color.Chocolate;
+            }
+        }
+    }
+}
